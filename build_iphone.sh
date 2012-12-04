cda9bbb952c27ae1810fa91f3e27bd8827382cae
Anthony Blake <anthonix@me.com>
2012-12-05 10:54:20 +1300

    Changed iphone build script

--- build_iphone.sh
+++ build_iphone.sh
@@ -7,7 +7,7 @@ INSTALL_DIR="`pwd`/build"
 export SDKVER="6.0"
 export DEVROOT="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer"
 export SDKROOT="$DEVROOT/SDKs/iPhoneOS$SDKVER.sdk"
-export CFLAGS="-O3 -Wreturn-type -Wparentheses -Wswitch -Wno-unused-parameter -Wno-unused-variable -Wunused-value -Wno-shorten-64-to-32 -Wno-trigraphs -fpascal-strings -miphoneos-version-min=5.0 -mcpu=cortex-a9 -arch armv7 -mfpu=neon -pipe -isysroot $SDKROOT -isystem $SDKROOT/usr/include -isystem $DEVROOT/usr/include -std=c99 -mno-thumb -no-integrated-as"
+export CFLAGS="-O3 -Wreturn-type -Wparentheses -Wswitch -Wno-unused-parameter -Wno-unused-variable -Wunused-value -Wno-shorten-64-to-32 -Wno-trigraphs -fpascal-strings -miphoneos-version-min=5.0 -mcpu=cortex-a9 -arch armv7 -mfpu=neon -pipe -isysroot $SDKROOT -isystem $SDKROOT/usr/include -isystem $DEVROOT/usr/include -mno-thumb -no-integrated-as"
 export AR="$DEVROOT/usr/bin/ar"
 export CC="clang"
 
