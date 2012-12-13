002abd4b984c9c9d2c2f3a8157e010eb76baf6ad
Anthony Blake <anthonix@me.com>
2012-12-14 00:08:52 +1300

    Temp hack to fix aligned stuff in ffts.h

--- src/ffts.h
+++ src/ffts.h
@@ -62,8 +62,13 @@ typedef struct _ffts_plan_t ffts_plan_t;
 
 struct _ffts_plan_t {
 	ptrdiff_t *offsets;
-	void  *ws;
+#ifdef DYNAMIC_DISABLED
+	void *ws;
 	void  *oe_ws, *eo_ws, *ee_ws;
+#else
+	void __attribute__((aligned(32))) *ws;
+	void __attribute__((aligned(32)))  *oe_ws, *eo_ws, *ee_ws;
+#endif
 	ptrdiff_t *is;
 	size_t *ws_is;
 	size_t i0, i1, n_luts;
